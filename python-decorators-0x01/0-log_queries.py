c60c86c615cb477f6e2004aed55b060e499b0cc8
Peter Oduor <peteropondo28@gmail.com>
2025-11-08 13:01:06 +0300

    Update 0-log_queries.py

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,10 +1,4 @@
-create a decorator that logs database queries executed by any function
 
-Instructions:
-
-Complete the code below by writing a decorator log_queries that logs the SQL query before executing it.
-
-Prototype: def log_queries()
 
 import sqlite3
 import functools
