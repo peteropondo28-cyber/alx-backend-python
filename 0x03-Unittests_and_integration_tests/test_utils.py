6a958eaa979c0968c8925090ef299bc672518677
Peter Oduor <peteropondo28@gmail.com>
2025-11-12 16:26:54 +0300

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,20 +1,73 @@
 #!/usr/bin/env python3
-import unittest
-from parameterized import parameterized
-from your_module import access_nested_map  # Replace your_module
+import requests
+from functools import wraps
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
 
-class TestAccessNestedMap(unittest.TestCase):
+__all__ = [
+    "access_nested_map",
+    "get_json",
+    "memoize",
+]
+
+
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Access nested map with key path.
+    Parameters
+    ----------
+    nested_map: Mapping
+        A nested map
+    path: Sequence
+        a sequence of key representing a path to the value
+    Example
+    -------
+    >>> nested_map = {"a": {"b": {"c": 1}}}
+    >>> access_nested_map(nested_map, ["a", "b", "c"])
+    1
     """
-    Unit tests for the access_nested_map function.
+    for key in path:
+        if not isinstance(nested_map, Mapping):
+            raise KeyError(key)
+        nested_map = nested_map[key]
+
+    return nested_map
+
+
+def get_json(url: str) -> Dict:
+    """Get JSON from remote URL.
     """
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-        ({"a": {"b": 0}}, ("a", "b"), 0),  # New test case: Expected 0
-    ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """
-        Tests the access_nested_map function with various inputs.
-        """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+    response = requests.get(url)
+    return response.json()
+
+
+def memoize(fn: Callable) -> Callable:
+    """Decorator to memoize a method.
+    Example
+    -------
+    class MyClass:
+        @memoize
+        def a_method(self):
+            print("a_method called")
+            return 42
+    >>> my_object = MyClass()
+    >>> my_object.a_method
+    a_method called
+    42
+    >>> my_object.a_method
+    42
+    """
+    attr_name = "_{}".format(fn.__name__)
+
+    @wraps(fn)
+    def memoized(self):
+        """"memoized wraps"""
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return property(memoized)
